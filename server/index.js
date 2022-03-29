a043239a047266bc8c2ed1a7d9da991ef8af776d
Owali Ullah Shawon <oshawon@gmail.com>
2022-03-29 13:37:40 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,74 @@
+import cors from 'cors';
+import express, { json } from 'express';
+import { query } from './config/db';
+
+const app = express();
+const PORT = 3002;
+app.use(cors());
+app.use(json())
+
+// Route to get all posts
+app.get("/api/get", (req, res) => {
+    query("SELECT * FROM posts", (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    });
+});
+
+// Route to get one post
+app.get("/api/getFromId/:id", (req, res) => {
+
+    const id = req.params.id;
+    query("SELECT * FROM posts WHERE id = ?", id,
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            }
+            res.send(result)
+        });
+});
+
+// Route for creating the post
+app.post('/api/create', (req, res) => {
+
+    const username = req.body.userName;
+    const title = req.body.title;
+    const text = req.body.text;
+
+    query("INSERT INTO posts (title, post_text, user_name) VALUES (?,?,?)", [title, text, username], (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(result)
+    });
+})
+
+// Route to like a post
+app.post('/api/like/:id', (req, res) => {
+
+    const id = req.params.id;
+    query("UPDATE posts SET likes = likes + 1 WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log(result)
+    });
+});
+
+// Route to delete a post
+
+app.delete('/api/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    query("DELETE FROM posts WHERE id= ?", id, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
